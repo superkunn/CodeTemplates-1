f1cdc09a188644edbb32af19581070d5cdd4e771
superkun <superkunn@users.noreply.github.com>
2018-07-12 22:18:57 +0800

    Update SPFA.cpp

--- Graph/Shortest Path Algorithms/SPFA.cpp
+++ Graph/Shortest Path Algorithms/SPFA.cpp
@@ -1,43 +1,64 @@
-#include<bits/stdc++.h>
-#define MAXV 1000
-#define MAXE 10000
-#define INF 1000000
+//poj 3168
+#include<iostream>
+#include<cstdio>
+#include<queue>
+#include<vector>
 using namespace std;
+const int MAXV=1e5+50;
+const int INF=1e9;
 struct edge{int to,cost;};
 typedef pair<int,int> P;
 int V;
 vector<edge> G[MAXV];
 int d[MAXV];
+int cnt[MAXV];
 bool inque[MAXV];
 queue<int> que;
-void spfa(int s)
-{
-    fill(d,d+V,INF);
-    fill(inque,inque+V,false);
-    d[s]=0;
+int spfa(int s){
+    fill(d,d+V+1,INF);
+    fill(cnt,cnt+V+1,0);
+    fill(inque,inque+V+1,false);
     while(!que.empty()) que.pop();
     que.push(s);
     inque[s]=true;
-    while(!que.empty())
-    {
+    d[s]=0;
+    cnt[s]=1;
+    while(!que.empty()){
         int u=que.front();
+        inque[u]=false;
         que.pop();
-        for(int i=0;i<G[u].size();i++)
-        {
-            if(d[u]+G[u][i].cost<d[G[u][i].to])
-            {
-                d[G[u][i].to]=d[u]+G[u][i].cost;
-                if(!inque[G[u][i].to])
-                {
-                    inque[G[u][i].to]=true;
-                    que.push(G[u][i].to);
+        for(int i=0;i<G[u].size();i++){
+            edge e=G[u][i];
+            if(d[u]+e.cost<d[e.to]){
+                d[e.to]=d[u]+e.cost;
+                if(!inque[e.to]){
+                    if(++cnt[e.to]>V)return -1;
+                    inque[e.to]=true;
+                    que.push(e.to);
                 }
             }
         }
-        inque[u]=false;
     }
+    return d[V];
 }
-int main()
-{
+int main(){
+    int a1,a2,a3;
+    scanf("%d%d%d",&a1,&a2,&a3);
+    V=a1;
+    for(int i=1;i<=V;i++){
+        G[i].push_back(edge{i-1,0});
+    }
+    for(int i=1;i<=a2;i++){
+        int x,y,z;
+        scanf("%d%d%d",&x,&y,&z);
+        G[x].push_back(edge{y,z});
+    }
+    for(int i=1;i<=a3;i++){
+        int x,y,z;
+        scanf("%d%d%d",&x,&y,&z);
+        G[y].push_back(edge{x,-z});
+    }
+    int w=spfa(1);
+    printf("%d",w==INF?-2:w);
     return 0;
 }
